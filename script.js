8803284a6c250bc63af3e71bb2a4dedd70729ecb
Alex Kuznetsov <a.kuznetsov23@gmail.com>
2022-02-10 01:21:36 +0200

    Игровой бот, но не работает в случае нажатия отмены

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 'use strict'
 
+const isNumber = function (num) {
+      return !isNaN(parseFloat(num)) && isFinite(num)
+}
+
+function gamebot (num) {
+      function guesscount () {
+      let countNumber = +prompt('Угадай число от 1 до 100')
+
+      if (countNumber > num) {
+            alert('Загаданное число меньше!')
+            guesscount()
+      } else if (countNumber < num) {
+            alert('Загаданное число больше!')
+            guesscount()
+      } else if (!isNumber(countNumber)) {
+            alert('Введи число!')
+            guesscount() 
+      } else if (countNumber == null) {
+            alert('Игра окончена')
+      } else if (countNumber == num) {
+            alert('Поздравляю, Вы угадали!')
+      }
+}
+      guesscount()
+}
+
+gamebot(10)
+
